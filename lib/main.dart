865205060a7f9bbd77f14dcc6338e2bc35338357
Gabriel Marques <gabrielmarquesmtv123@gmail.com>
2022-03-03 13:45:35 -0300

    pós feriado

--- lib/main.dart
+++ lib/main.dart
@@ -15,15 +15,30 @@ class _PerguntaAppState extends State<PerguntaApp> {
   final List<Map<String, Object>> _perguntas = const [
     {
       'texto': 'Qual sua cor favorita?',
-      'respostas': ['Azul', 'Vermelho', 'Preto', 'Verde'],
+      'respostas': [
+        {'texto': 'Azul', 'nota': 10},
+        {'texto': 'Vermelho', 'nota': 20},
+        {'texto': 'Preto', 'nota': 25},
+        {'texto': 'Verde', 'nota': 40}
+      ],
     },
     {
       'texto': 'Qual o seu animal favorito?',
-      'respostas': ['Cachorro', 'Gato', 'Peixe', 'Tartaruga'],
+      'respostas': [
+        {'texto': 'Cachorro', 'nota': 53},
+        {'texto': 'Gato', 'nota': 23},
+        {'texto': 'Peixe', 'nota': 60},
+        {'texto': 'Tartaruga', 'nota': 45},
+      ],
     },
     {
       'texto': 'Qual o seu professor favorito?',
-      'respostas': ['Dory', 'Fred', 'Marllos', 'Carlos'],
+      'respostas': [
+        {'texto': 'Dory', 'nota': 80},
+        {'texto': 'Fred', 'nota': 54},
+        {'texto': 'Marllos', 'nota': 65},
+        {'texto': 'Carlos', 'nota': 32},
+      ],
     },
   ];
 
